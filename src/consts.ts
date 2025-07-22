c25badb85f8aa3557de11a9e185fbb542b25435e
Paige Yoo <paigeyoo@MacBook-Pro-4.local>
2025-07-22 15:16:56 -0700

    add navbar

--- src/consts.ts
+++ src/consts.ts
@@ -10,14 +10,13 @@ export const ABOUT_ME =
   `Token is a modern and minimalistic Astro + Tailwind CSS starter designed for developers, students, and creators looking to build clean, fast, and functional websites. Featuring a sleek black-and-white theme with a custom terminal-inspired font, this template is perfect for personal portfolios, blogs, or any static content-driven website. The template comes packed with features like Markdown (MD) and MDX support for writing blog posts or documentation, allowing for flexibility and ease of use. It includes Tailwind CSS for effortless styling and customization, enabling users to tweak the design to match their branding or personal preferences without hassle. Additionally, Token integrates essential utilities like RSS feed generation, a sitemap for SEO optimization, and pre-configured components for seamless content management. Everything is static, making the site incredibly fast, lightweight, and easy to deploy on platforms like Vercel, Netlify, or Deno Deploy. Whether you're a programmer showcasing your projects, a writer sharing your ideas, or someone looking to establish an online presence, this template provides all the tools you need in a stylish and functional package. With its developer-first approach and clean codebase, Token offers a strong foundation for creating visually appealing websites that stand out while keeping performance and simplicity at the forefront.`.trim();
 export const GITHUB_USERNAME = "py1218";
 export const QUOTE = "Programmer, Gamer, Editor and Student";
-export const NAV_LINKS: Array<{ title: string; href?: string }> = [
+export const NAV_LINKS: Array<{ title: string; href?: string; directory: string; }> = [
   {
-    title: "Play",
+    title: "Play (Coming Soon)",
+    directory: "play",
   },
   {
-    title: "About Us",
-  },
-  {
-    title: "Contact",
+    title: "About",
+    directory: "about",
   },
 ];
